8eb400cb724bf4d4199fe48fba60623ffa9e1c94
Singh <mukesh.d.singh@capgemini.com>
2026-03-02 12:46:20 +0530

    Create .NET Web API with Swagger and HelloWorldController

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorld.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HelloWorldController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Hello World");
+    }
+}
